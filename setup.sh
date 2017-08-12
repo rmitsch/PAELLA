9c4f398d00bbe15dd94f08f3148302c033055570
Raphael Mitsch <laarem33@hotmail.com>
2017-08-12 20:47:35 +0200

    Update requirements.txt after removing lda2vec as dependency.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# @author RM
-# @date 2017-08-11
-# Additional setup necessary for TOPAC.
-
-## Set up lda2vec.
-# Execute install script.
-python lda2vec/setup.py install
-# Download spacy's english language model.
-sudo python -m spacy.en.download
